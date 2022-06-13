0651129729b192083931ca38ba08c7554fdc397a
Bruno Alvim <brunocarvalhoalvim@gmail.com>
2022-06-12 21:14:21 -0300

    agora o codigo mostra o local do executavel e o sistema em que está sendo executado

--- labs/python/datetime/ano-do-linux.py
+++ labs/python/datetime/ano-do-linux.py
@@ -1,10 +1,14 @@
 from datetime import date
+import sys
+import platform
 
 
 ano = date.today()
-
+executavel = sys.executable
+sistema = platform.uname()
 ano_do_linux = ano.replace(year=2023)
 
 print(f'Em {ano} inicia-se o ano do linux')
 
 print(f'caso de tudo errado o ano do linux se iniciará em {ano_do_linux}')
+print(f'Esse script de python esta sendo executado em {executavel} em um {sistema.system}')
