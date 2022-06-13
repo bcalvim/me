09a8fe5455f288420b316967e1699f5157ad4f5e
Bruno Alvim <brunocarvalhoalvim@gmail.com>
2022-06-12 21:26:25 -0300

    novo teste

--- labs/python/datetime/ano-do-linux.py
+++ labs/python/datetime/ano-do-linux.py
@@ -8,7 +8,11 @@ executavel = sys.executable
 sistema = platform.uname()
 ano_do_linux = ano.replace(year=2023)
 
+if sistema.system == 'Windows':
+    print('Estranho, estamos rodando num Windows')
+else:
+    print('Eba, estamos em um Linux')
+
 print(f'Em {ano} inicia-se o ano do linux')
 
 print(f'caso de tudo errado o ano do linux se iniciará em {ano_do_linux}')
-print(f'Esse script de python esta sendo executado em {executavel} em um {sistema.system}')
